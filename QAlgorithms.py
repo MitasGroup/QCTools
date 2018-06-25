89acab2a037f3b69c9d0db05fa5f9028dcb32125
M. Chandler Bennett <mcbennet@ncsu.edu>
2018-06-25 14:21:02 -0400

    Adding ipea to QAlgorithms.py

--- QAlgorithms.py
+++ QAlgorithms.py
@@ -18,7 +18,7 @@ def qift(q,Q):
             Q.cu1(-2.0*np.pi/2**(j+1),q[j],q[i])
         Q.h(q[i])
 
-#phase_estimation algorithm with controlled unitary function
+#phase estimation algorithm with controlled unitary function
 def phase_estimation(qs,qr,cr,Q,controlled_unitary,*args):
     #apply hadamard to each readout qubit
     Q.h(qr)
@@ -34,3 +34,42 @@ def phase_estimation(qs,qr,cr,Q,controlled_unitary,*args):
     #measure the readout qubits
     for r in range(len(qr)):
         Q.measure(qr[r],cr[r])
+    
+    # Potentially we output or return the result here
+
+#iterative phase estimation algorithm with controlled unitary function
+def iterative_phase_estimation(qs,qr,cr,Q,controlled_unitary,accuracy,*args):
+    #previous_bits=[1,0,0,0,0,0,0,1]
+    phase_bits=[]
+    phase_factor=0
+
+    #apply hadamard to each readout qubit
+    Q.h(qr)
+
+    n=accuracy-1
+    while n >= 0:
+
+        # Global phase
+        Q.u1(-2*pi*phase_factor,qr[0])
+
+        #apply controlled unitary gates
+        for r in range(len(qr)):
+            for i in range(2**n):
+                print(i,r)
+                controlled_unitary(qs,qr[r],Q,*args)
+        #apply inverse QFT to readout qubits
+        qift(qr,Q)
+
+        #measure the readout qubits
+        for r in range(len(qr)):
+            Q.measure(qr[r],cr[r])
+   
+        # Need to add the bit values with maximum count to phase_bits
+
+        # Generate global phase factor 
+        for i in range(n,n-len(qr),-1):
+            phase_factor+=previous_bits[i]/(2**(2+i))
+
+        n-=len(qr)
+
+    # Potentially we output or return the result here
