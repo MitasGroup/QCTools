dda5e9d0ae4e65964ce4de7ccbb1b98d79b633c8
Cody Melton <camelto2@ncsu.edu>
2018-06-26 10:54:44 -0400

    Rearragement
    I think that dec2bin should be in the helper, not the gates
    I also removed my old QIFT.

--- Gates.py
+++ Gates.py
@@ -1,17 +1,6 @@
 #! /usr/bin/env python3
 import math
 
-#decimal to binary converter
-def dec2bin(val,fracbits=32):
-    whole = int(val)
-    frac = val-whole
-    ret = format(whole,"b")+"."
-    for i in range(32):
-        whole = int(frac*2)
-        ret+=str(whole)
-        frac=frac*2-whole
-    return ret
-
 #swap all qubits in register
 def swap_all(qc,r):
     for i in range(math.floor(len(r)/2)):
@@ -29,15 +18,6 @@ def qft(q,Q):
             Q.cu1(2.0*math.pi/2**(j+1),q[j],q[i])
     for i in range(int(math.floor(len(q)/2))):
         Q.swap(q[i],q[int(len(q))-1-i])
-
-#quantum inverse fourier transform
-def qift(q,Q):
-    for i in range(int(math.floor(len(q)/2))):
-        Q.swap(q[i],q[int(len(q))-1-i])
-    for i in reversed(range(len(q))):
-        for j in reversed(range(i+1,len(q))):
-            Q.cu1(-2.0*math.pi/2**(j+1),q[j],q[i])
-        Q.h(q[i])
 
 def iqft(qc,r):
     nqubits=len(r)
