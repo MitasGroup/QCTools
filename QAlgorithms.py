b98e4d555dbc7ab8037d30b7cd7a1f086158bb7d
M. Chandler Bennett <mcbennet@ncsu.edu>
2018-06-26 08:05:30 -0400

    Small changes to QAlgorithms.py

--- QAlgorithms.py
+++ QAlgorithms.py
@@ -25,8 +25,8 @@ def iterative_phase_estimation(qs,qr,cr,Q,controlled_unitary,accuracy,*args):
     phase_factor=0
     rlen=len(qr) 
 
-    n=accuracy-1
-    while n >= 0:
+    n=accuracy
+    while n > 0:
         #apply global phase
         Q.u1(-2*math.pi*phase_factor,qr[0])
 
@@ -35,7 +35,7 @@ def iterative_phase_estimation(qs,qr,cr,Q,controlled_unitary,accuracy,*args):
 
         #apply controlled unitary gates
         for r in range(rlen):
-            for i in range(2**(n-r)):
+            for i in range(2**(n-1-r)):
                 print(i,r)
                 controlled_unitary(qs,qr[r],Q,*args)
         #apply inverse QFT to readout qubits
@@ -47,7 +47,7 @@ def iterative_phase_estimation(qs,qr,cr,Q,controlled_unitary,accuracy,*args):
         # Need to add the bit values with maximum count to phase_bits
 
         #add to global phase factor 
-        for i in range(n,n-rlen,-1):
+        for i in range(n-1,n-1-rlen,-1):
             phase_factor+=phase_bits[i]/(2**(2+i))
 
         n-=rlen
