3424b542edeb3a3ff7bebe285d03b017f5503eb5
M. Chandler Bennett <mcbennet@ncsu.edu>
2018-06-29 22:45:01 -0400

    Updating ipea

--- QAlgorithms.py
+++ QAlgorithms.py
@@ -4,7 +4,7 @@ from QCTools import Gates, QHelperFunctions
 import operator
 
 #phase estimation algorithm with controlled unitary function
-def phase_estimation(qs,qr,cr,Q,controlled_unitary,*args):
+def phase_estimation(qs,qr,cs,cr,Q,controlled_unitary,*args):
     #apply hadamard to each readout qubit
     Q.h(qr)
 
@@ -15,6 +15,7 @@ def phase_estimation(qs,qr,cr,Q,controlled_unitary,*args):
     #apply inverse QFT to readout qubits
     Gates.iqft(Q,qr)
 
+    #x=4
     #measure the readout qubits
     for r in range(len(qr)):
         Q.measure(qr[r],cr[r])
@@ -22,35 +23,34 @@ def phase_estimation(qs,qr,cr,Q,controlled_unitary,*args):
     # Potentially we output or return the result here
 
 #iterative phase estimation algorithm with controlled unitary function
-def iterative_phase_estimation(qs,qr,cr,Q,backend,shots,rsize,controlled_unitary,accuracy,*args):
+def iterative_phase_estimation(qs0,qr0,cs0,cr0,Q0,backend,shots,rsize,controlled_unitary,accuracy,*args):
     from qiskit import QuantumCircuit, ClassicalRegister, QuantumRegister
     from qiskit import execute, compile
     import re
+    import copy
 
     bit_str=''
     phase_factor=0
-    rlen=len(qr) 
-    slen=len(qs) 
+    rlen=len(qr0) 
+    slen=len(qs0) 
+
+    print("rlen: ",rlen)
+    print("acc: ",accuracy)
 
     n=accuracy
     while n > 0:
         print('Current phase estimate is  0.'+'x'*(n)+str(bit_str))
         
-        # Create a Quantum Register with 2 qubits.
-        qr = QuantumRegister(rlen)
-        qs = QuantumRegister(slen)
+        # Re-initialize Quantum Registers.
+        qr = copy.deepcopy(qr0) 
+        qs = copy.deepcopy(qs0) 
 
-        # Create a Classical Register with 2 bits.
-        cr = ClassicalRegister(rlen)
-        cs = ClassicalRegister(slen)
+        # Create a Classical Registers.
+        cr = copy.deepcopy(cr0) 
+        cs = copy.deepcopy(cs0) 
 
-        # Create a Quantum Circuit
-        Q = QuantumCircuit(qr, qs, cr, cs)
-
-        # Prepare HF ground state. 0th and 1st orbs occupied. 
-        # This is H2 specific and needs to be generalized
-        Q.x(qs[0]) 
-        Q.x(qs[1]) 
+        # Re-initialize Quantum Circuit
+        Q = copy.deepcopy(Q0) 
 
         #apply hadamard to each readout qubit
         Q.h(qr)
@@ -93,5 +93,3 @@ def iterative_phase_estimation(qs,qr,cr,Q,backend,shots,rsize,controlled_unitary
         n=n-rlen
 
     print('Final phase estimate is  0.'+str(bit_str))
-
-    # Potentially we output or return the result here
